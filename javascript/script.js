8ee83fa0c2f8b57c3755b7c8bc807099dae2f6a3
rubacodes <rubacodes@gmail.com>
2022-05-02 17:21:13 +0200

    sperata la creazione del tile dal suo tipo

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,25 @@
 
 // hook alla lista
-const ul = document.querySelector("ul.box");
+const ul = document.querySelector('ul.box');
 
 //stampa in console i numeri da 1 a 100
 for (i = 1; i < 100; i++) {
     //creo element lista
     const li = document.createElement('li');
+    li.className = 'tile';
     // 3- fizz ;5-buzz
     if ((i % 3 === 0) && (i % 5 === 0)) { //multiplo di 3 e 5
         console.log("FizzBuzz");
-        li.className = "tile fizz buzz";
+        li.classList.add('fizz');
+        li.classList.add('buzz');
         li.append("FizzBuzz");
     } else if ((i % 3 === 0) && (i % 5 !== 0)) {  //multiplo  solo di 3 
         console.log("Fizz");
-        li.className = "tile fizz";
+        li.classList.add('fizz')
         li.append("Fizz");
     } else if ((i % 3 !== 0) && (i % 5 === 0)) {  //multiplo solo di 5
         console.log("Buzz");
-        li.className = "tile buzz";
+        li.classList.add('buzz')
         li.append("Buzz");
     }
     else { //ne multiplo di 3 ne di 5
