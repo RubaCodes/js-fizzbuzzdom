6c705094eb478935e5acfd81eea2d4cfa8630d57
rubacodes <rubacodes@gmail.com>
2022-05-02 13:53:55 +0200

    inserita logica fizz/buzz

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,14 @@
 //stampa in console i numeri da 1 a 100
 for (i = 1; i <= 100; i++){
-    console.log(i);
+    // 3- fizz ;5-buzz
+    if((i%3 ===0) && (i%5 ===0)){
+        console.log("fizz buzz");
+    }else if((i%3 ===0) && (i%5 !==0)){
+        console.log("fizz");
+    }else if((i%3 !==0) && (i%5 ===0)){
+        console.log("buzz");
+    }
+    else{
+        console.log(i);
+    }
 }
