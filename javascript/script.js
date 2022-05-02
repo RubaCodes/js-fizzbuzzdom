b45a46579ee9ae4ed04e974ea7b5a5bf7230dcf5
rubacodes <rubacodes@gmail.com>
2022-05-02 14:22:24 +0200

    implementata logica tiles

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,33 @@
+
+// hook alla lista
 const ul = document.querySelector("ul.box");
 console.log(ul);
 
-
-
-
 //stampa in console i numeri da 1 a 100
-for (i = 1; i <= 100; i++){
+for (i = 1; i < 100; i++) {
+    //creo element lista
+    const li = document.createElement('li');
     // 3- fizz ;5-buzz
-    if((i%3 ===0) && (i%5 ===0)){
+    if ((i % 3 === 0) && (i % 5 === 0)) {
         console.log("FizzBuzz");
-    }else if((i%3 ===0) && (i%5 !==0)){
+        li.className = "tile fizz buzz";
+        li.append("FizzBuzz");
+        ul.append(li);
+    } else if ((i % 3 === 0) && (i % 5 !== 0)) {
         console.log("Fizz");
-    }else if((i%3 !==0) && (i%5 ===0)){
+        li.className = "tile fizz";
+        li.append("Fizz");
+        ul.append(li);
+    } else if ((i % 3 !== 0) && (i % 5 === 0)) {
         console.log("Buzz");
+        li.className = "tile bizz";
+        li.append("Buzz");
+        ul.append(li);
     }
-    else{
+    else {
         console.log(i);
+        li.className = "tile";
+        li.append(i);
+        ul.append(li);
     }
 }
